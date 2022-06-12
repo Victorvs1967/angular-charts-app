c8ed21e2e3a0cda40c8d28c43d2a05df53018506
Victor <victorsmirnov@me.com>
2022-06-12 14:41:41 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-charts-app';
+
+  saleData = [
+    { name: 'Mobiles', value: 105000 },
+    { name: 'Notebook', value: 55000 },
+    { name: 'AC', value: 15000 },
+    { name: 'Headset', value: 150000 },
+    { name: 'Fridge', value: 20000 },
+  ]
 }
